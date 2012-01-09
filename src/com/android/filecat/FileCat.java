1844b290212f4e3871d507d1b847d69ff39343f6
Adam Marchetti <adamnew123456@gmail.com>
2012-01-08 22:07:03 -0500

    Support for large files.
     - App is now multithreaded, avoiding
       process exit due to timeouts.
     - Networking system dumps data every
       chunk, keeping the app from consuming
       too much space.

--- src/com/android/filecat/FileCat.java
+++ src/com/android/filecat/FileCat.java
@@ -4,15 +4,111 @@ import java.io.*;
 
 import android.app.*;
 import android.content.*;
+import android.os.*;
 import android.view.*;
 import android.util.*;
 import android.widget.*;
 
-import android.os.Bundle;
-
 public class FileCat extends Activity
 {
     EditText e_ipaddr, e_port, e_filename;
+    TextView t_log;
+    
+    class SendTask extends AsyncTask<Object,Void,Boolean>
+    {
+        private TextView logging;
+        private String s_ipaddr, s_port, s_filename;
+
+        protected SendTask(TextView tv, EditText ipaddr, EditText port, EditText filename)
+        {
+            super();
+            logging = tv;
+            s_ipaddr = ipaddr.getText().toString();
+            s_port = port.getText().toString();
+            s_filename = filename.getText().toString();
+        }
+
+        protected void onPreExecute()
+        {
+            logging.append("Starting... ");
+        }
+
+        protected void onPostExecute(Boolean success)
+        {
+            if (success.booleanValue())
+            {
+                logging.append("Succeeded\n");
+            }
+            else
+            {
+                logging.append("Failed\n");
+            }
+        }
+
+        protected Boolean doInBackground(final Object... args)
+        {
+            boolean success = true;
+            try
+            {
+                int port = Integer.parseInt(s_port);
+                Networking.do_write(s_ipaddr, port, s_filename);
+            }
+            catch (Exception e)
+            {
+                success = false;
+            }
+
+            return new Boolean(success);
+        }
+    }
+
+    class ReadTask extends AsyncTask<Object,Void,Boolean>
+    {
+        private TextView logging;
+        private String s_ipaddr, s_port, s_filename;
+
+        protected ReadTask(TextView tv, EditText ipaddr, EditText port, EditText filename)
+        {
+            super();
+            logging = tv;
+            s_ipaddr = ipaddr.getText().toString();
+            s_port = port.getText().toString();
+            s_filename = filename.getText().toString();
+        }
+
+        protected void onPreExecute()
+        {
+            logging.append("Starting... ");
+        }
+
+        protected void onPostExecute(Boolean success)
+        {
+            if (success.booleanValue())
+            {
+                logging.append("Succeeded\n");
+            }
+            else
+            {
+                logging.append("Failed\n");
+            }
+        }
+
+        protected Boolean doInBackground(final Object... args)
+        {
+            boolean success = true;
+            try 
+            {
+                int port = Integer.parseInt(s_port);
+                Networking.do_read(port, s_filename);
+            }
+            catch (Exception e)
+            {
+                success = false;
+            }
+
+            return new Boolean(success);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -24,38 +120,22 @@ public class FileCat extends Activity
         e_port = (EditText)findViewById(R.id.port);
         e_filename = (EditText)findViewById(R.id.fname);
 
+        t_log = (TextView)findViewById(R.id.log);
+
         Button b_send = (Button)findViewById(R.id.sendfile);
         Button b_recv = (Button)findViewById(R.id.recvfile);
 
         b_send.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)    
+            public void onClick(View v)
             {
-                try
-                {
-                    int port = Integer.parseInt(e_port.getText().toString());
-                    Networking.do_write(e_ipaddr.getText().toString(), 
-                            port, 
-                            e_filename.getText().toString());
-                }
-                catch (Exception e)
-                {
-                    Log.e("FileCat", e.toString());
-                }
+                new SendTask(t_log, e_ipaddr, e_port, e_filename).execute();
             }
         });
 
         b_recv.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                try 
-                {
-                    int port = Integer.parseInt(e_port.getText().toString());
-                    Networking.do_read(port, e_filename.getText().toString());
-                }
-                catch (Exception e)
-                {
-                    Log.e("FileCat", e.toString());
-                }
+                new ReadTask(t_log, e_ipaddr, e_port, e_filename).execute();
             }
         });
     }
